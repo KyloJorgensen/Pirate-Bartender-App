709ed14495dd8a46aedd8a53f1d7dfed0d2c2031
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-06-28 12:14:10 -0600

    did an overhall on the app, the bartender Const works now

--- js/app.js
+++ js/app.js
@@ -1,82 +1,96 @@
 $(document).ready(function() {
 	"use strict";
+
+	$('body').on('click', '.fa-thumbs-o-down', function() {
+		$(this).removeClass('fa-thumbs-o-down').addClass('fa-thumbs-o-up');
+	})
+	.on('click', '.fa-thumbs-o-up', function() {
+		$(this).removeClass('fa-thumbs-o-up').addClass('fa-thumbs-o-down');
+	})
+	.on('click', '.mix-drink', function() {
+		ben.createDrink()
+	})
+	.on('click', '.drink', function() {
+		ben.generateQuestions();
+	})
+	.on('click', '.main-menu', function() {
+		ben.mainMenu();
+	});
+});
 	// Constructor object for list of questions
 	var Bartender = function(questions) {
 		this.questions = questions;
 	};
-	//  questions object made from Constructor ListofQuestions
-	var questions = new Bartender(data.questions);
-	console.log(questions.questions);
-	// Constructor object for list of ingredents
-	var Ingredient = function(ingredients) {
-		this.ingredients = ingredients;
+
+	Bartender.prototype.generateQuestions = function() {
+		$('.questions').empty();
+		questions = this.questions;
+		for (var i = 0; i < questions.length; i++) {
+			$('.questions').append('<li><h1>' + questions[i].question + '</h1><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></li>');	
+		};
+		$('.questions').append('<li><button class="mix-drink">Mix Drink</button></li>')
 	};
-	// ingredients object made frin Ingredient Constructor
-	var ingredients = new Ingredient(data.ingredients);
-	console.log(ingredients.ingredients.fruityIngredients);
+
+	Bartender.prototype.createDrink = function() {
+		var ingredients = [],
+		drink = '';
+		questions = this.questions;
+		$('.fa-thumbs-o-up').each(function() {
+			for (var i = 0; i < questions.length; i++) {
+				if (questions[i].question == $(this).parent().children('h1').html()) {
+					var ingredient = new Ingredient(questions[i]);
+					ingredient.prototype = Ingredient.prototype;
+					ingredient.randomIngredient();
+					ingredients.push(ingredient.ingredient);
+				}
+			}
+
+		});
+
+		for (var i = 0; i < ingredients.length; i++) {
+			if (ingredients.length == 1 ) {
+				drink += ingredients[i];
+			} else if (i == ingredients.length - 1) {
+				drink += 'with ' + ingredients[i];
+			} else {
+				drink += ingredients[i] + ', ';
+			}
+		}
+		console.log(drink);
+		this.generateDrink(drink)
+	};
+
+	Bartender.prototype.generateDrink = function(drink) {
+		$('.questions').empty().append('<li><h1>' + drink + '</h1></li><li><button class="main-menu">Anything Else?</button></li>');
+	};
+
+	Bartender.prototype.mainMenu = function() {
+		$('.questions').empty().append('<li><button class="drink">Drink?</button></li><li><button class="burger">Burger?</button></li>');
+	};
+	
+	// Constructor object randomly select an ingredient from a list of ingredients
+	var Ingredient = function(ingredients) {
+		this.ingredients = ingredients.ingredients.choices;
+		this.ingredient = 'None';
+	};
+
+	Ingredient.prototype.randomIngredient = function() {
+		this.ingredient = this.ingredients[Math.floor((Math.random() * this.ingredients.length))];
+	};
+
+	var ben = new Bartender(data);
+	ben.prototype = Bartender.prototype;
+	
+
 
 	// Constructor object for list of all of the ingredients
 	var Pantry = function(ingredients) {
-		this.ingredients = Object.keys(ingredients);
-		this.allofTheIngredients = function(ingredients) {
-			var arrayOfIngredents = [];
-			$.each(ingredients, function(index, value) {
-				$.each(value, function(index,value) {
-					arrayOfIngredents.push(value);
-				});
-			});
-			return arrayOfIngredents;
-		};
-	};
-	var pantry = new Pantry(data.ingredients);
-	console.log(pantry.ingredients);
-	console.log(pantry.allofTheIngredients(data.ingredients));
- 
-	// listens for user to push a button then checks what they pushed
-	$('li').on('click', 'button', function() {
-		validateInput($(this).html());
-	});
 
-	// determines what the user has pushed
-	function validateInput(input) {
-		if (input == 'No') {
-			$('.chocies').empty();
-			$('.question').text('Good luck bucko')
-		} else {
-			nextQuestion(input);
-		}
 	};
 
-	function nextQuestion(input) {
-		var currentQuestion = determineCurrentQuestion($('.question').html());
-		console.log(currentQuestion);
-		if (currentQuestion < data.questions.length) {
-			generateQuestion(currentQuestion);
-		} else {
 
-		}
-	}
 
-	function determineCurrentQuestion(input) {
-		for (var i = 0; i < data.questions.length; i++) {
-			if (data.questions[i] == input) {
-				console.log(data.questions[i]);
-				return i;
-			}
-		}
-		console.log('no match')
-		return 0;
-	}
 
-	function generateQuestion(currentQuestion) {
-		$('.question').text(data.questions[currentQuestion]);
-		$('.chocies').empty();
-		$.each(data.ingredients[currentQuestion], function(index, value) {
-			$('.chocies').append('<li><button>' + value + '</button></li>');
-		});
-	}
-
-});
 
 
 
--- js/data.js
+++ js/data.js
@@ -1,37 +1,59 @@
 // object with all of the data
-var data = { 
-	"questions": [
-        "Do ye like yer drinks strong?",
-        "Do ye like it with a salty tang?",
-        "Are ye a lubber who likes it bitter?",
-        "Would ye like a bit of sweetness with yer poison?",
-        "Are ye one for a fruity finish?"
-    ],
-    "ingredients": {
-        "strongIngredients": [
-            "glug of rum",
-            "slug of whisky",
-            "splash of gin"
-        ],
-        "saltyIngredients": [
-            "olive on a stick",
-            "salt-dusted rim",
-            "rasher of bacon"
-        ],
-        "bitterIngredients": [
-            "shake of bitters",
-            "splash of tonic",
-            "twist of lemon peel"
-        ],
-        "sweetIngredients": [
-            "sugar cube",
-            "spoonful of honey",
-            "splash of cola"
-        ],
-        "fruityIngredients": [
-            "slice of orange",
-            "dash of cassis",
-            "cherry on top"
-        ]
+var drink = [],
+data = [
+    {
+        question: "Do ye like yer drinks strong?",
+        ingredients: {         
+            "type": "strong ingredients",
+            "choices": [
+                "glug of rum",
+                "slug of whisky",
+                "splash of gin"
+            ]
+        }
+    },
+    {
+        question: "Do ye like it with a salty tang?",
+        ingredients: {
+            "type": "salty ingredients",
+            "choices": [
+                "olive on a stick",
+                "salt-dusted rim",
+                "rasher of bacon"
+            ]
+        }
+    },
+    {
+        question: "Are ye a lubber who likes it bitter?",
+        ingredients: {
+            "type": "bitter ingredients",
+            "choices": [
+                "shake of bitters",
+                "splash of tonic",
+                "twist of lemon peel"
+            ]
+        }
+    },
+    {
+        question: "Would ye like a bit of sweetness with yer poison?",
+        ingredients: {
+            "type": "sweet ingredients",
+            "choices": [
+                "sugar cube",
+                "spoonful of honey",
+                "splash of cola"
+            ]
+        }
+    },
+    {
+        question: "Are ye one for a fruity finish?",
+        ingredients: {
+            "type": "fruity ingredients",
+            "choices": [
+                "slice of orange",
+                "dash of cassis",
+                "cherry on top"
+            ]
+        }
     }
-};
+];
